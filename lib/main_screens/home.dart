86a8f9e7391ba7986f73a7e0ccbab3f15e8ff5bd
eduardolluis <1132907@est.intec.edu.do>
2026-03-27 09:40:13 -0400

    Search Bar

--- lib/main_screens/home.dart
+++ lib/main_screens/home.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_store_app/minor_screens/search.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,58 +11,101 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
+    return DefaultTabController(
       length: 9,
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
-          title: const CupertinoSearchTextField(),
-          bottom:  TabBar( 
+          title: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SearchScreen()),
+              );
+            },
+            child: Container(
+              height: 35,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.yellow, width: 1.4),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Icon(Icons.search, color: Colors.grey),
+                      ),
+                      Text(
+                        "What are you looking for?",
+                        style: TextStyle(color: Colors.grey, fontSize: 18),
+                      ),
+                    ],
+                  ),
+
+                  Container(
+                    alignment: Alignment.center,
+                    height: 32,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Text(
+                      "Search",
+                      style: TextStyle(color: Colors.grey, fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          bottom: TabBar(
             indicatorColor: Colors.yellow,
             indicatorWeight: 5,
             isScrollable: true,
             tabs: [
-              RepeatedTab(label: 'Men'),   
-              RepeatedTab(label: 'Women'),   
-              RepeatedTab(label: 'Shoes'), 
-              RepeatedTab(label: 'Bags'),   
-              RepeatedTab(label: 'Electronics'),   
-              RepeatedTab(label: 'Accessories'),   
-              RepeatedTab(label: 'Home & Garden'),   
-              RepeatedTab(label: 'Kids'),   
-              RepeatedTab(label: 'Beauty'),   
-
-
-
-          ],)
+              RepeatedTab(label: 'Men'),
+              RepeatedTab(label: 'Women'),
+              RepeatedTab(label: 'Shoes'),
+              RepeatedTab(label: 'Bags'),
+              RepeatedTab(label: 'Electronics'),
+              RepeatedTab(label: 'Accessories'),
+              RepeatedTab(label: 'Home & Garden'),
+              RepeatedTab(label: 'Kids'),
+              RepeatedTab(label: 'Beauty'),
+            ],
+          ),
         ),
-        body: TabBarView(children: [
-          Center(child: Text('men screen'),),
-          Center(child: Text('women screen'),),
-          Center(child: Text('shoes screen'),),
-          Center(child: Text('Bags screen'),),
-          Center(child: Text('Electronics screen'),),
-          Center(child: Text('Accessories screen'),),
-          Center(child: Text('Home & Garden screen'),),
-          Center(child: Text('Kids screen'),),
-          Center(child: Text('Beauty screen'),),
-
-        ]
+        body: TabBarView(
+          children: [
+            Center(child: Text('men screen')),
+            Center(child: Text('women screen')),
+            Center(child: Text('shoes screen')),
+            Center(child: Text('Bags screen')),
+            Center(child: Text('Electronics screen')),
+            Center(child: Text('Accessories screen')),
+            Center(child: Text('Home & Garden screen')),
+            Center(child: Text('Kids screen')),
+            Center(child: Text('Beauty screen')),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
 
 class RepeatedTab extends StatelessWidget {
   final String label;
-  const RepeatedTab({
-    super.key,
-    required this.label,
-  });
+  const RepeatedTab({super.key, required this.label});
 
   @override
-  Widget build(BuildContext context, ) {
-    return Tab(child: Text(label, style: TextStyle(color: Colors.grey[600]),),);
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Text(label, style: TextStyle(color: Colors.grey[600])),
+    );
   }
 }
