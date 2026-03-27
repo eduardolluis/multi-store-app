18249ff454e6340da4ab66f599cec9007ca009e5
eduardolluis <1132907@est.intec.edu.do>
2026-03-27 11:58:52 -0400

    adding slidebar

--- lib/categories/men_categ.dart
+++ lib/categories/men_categ.dart
@@ -7,56 +7,124 @@ class MenCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Text(
-            "Men",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.5,
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Text(
+                      "Men",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.68,
+                    child: GridView.count(
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 5,
+                      crossAxisCount: 3,
+                      childAspectRatio: 0.8,
+                      children: List.generate(men.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SubcategProducts(
+                                  maincategoryName: 'men',
+                                  subcategoryName: men[index],
+                                ),
+                              ),
+                            );
+                          },
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 70,
+                                width: 70,
+                                child: Image(
+                                  image: AssetImage('images/men/men$index.jpg'),
+                                ),
+                              ),
+                              Text(men[index]),
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.68,
-          child: GridView.count(
-            mainAxisSpacing: 70,
-            crossAxisSpacing: 15,
-            crossAxisCount: 3,
-            children: List.generate(men.length, (index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SubcategProducts(
-                        maincategoryName: 'men',
-                        subcategoryName: men[index],
-                      ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.05,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 40),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.brown.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: RotatedBox(
+                    quarterTurns: 3,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          ' << ',
+                          style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 10,
+                          ),
+                        ),
+                        Text(
+                          'men'.toUpperCase(),
+                          style: const TextStyle(
+                            color: Colors.brown,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 10,
+                          ),
+                        ),
+                        Text(
+                          ' >> '.toUpperCase(),
+                          style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 10,
+                          ),
+                        ),
+                      ],
                     ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 70,
-                      width: 70,
-                      child: Image(
-                        image: AssetImage('images/men/men$index.jpg'),
-                      ),
-                    ),
-                    Text(men[index]),
-                  ],
+                  ),
                 ),
-              );
-            }),
+              ),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
