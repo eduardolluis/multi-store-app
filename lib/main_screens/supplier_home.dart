ca38f146a11b3d56b0c7744b8b29b70b8bebba13
eduardolluis <1132907@est.intec.edu.do>
2026-03-29 14:21:07 -0400

    supplier home screen

---
+++ lib/main_screens/supplier_home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:multi_store_app/main_screens/category.dart';
+import 'package:multi_store_app/main_screens/home.dart';
+import 'package:multi_store_app/main_screens/stores.dart';
+
+class SupplierHomeScreen extends StatefulWidget {
+  const SupplierHomeScreen({super.key});
+
+  @override
+  State<SupplierHomeScreen> createState() => _SupplierHomeScreenState();
+}
+
+class _SupplierHomeScreenState extends State<SupplierHomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _tabs = [
+    HomeScreen(),
+    CategoryScreen(),
+    StoresScreen(),
+    Center(child: Text('Dashboard Screen')),
+    Center(child: Text('Upload Screen')),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Category'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Stores'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.upload), label: 'Upload'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
