c07dd94146457422ceffd37c6ed3ccb17ecdc8fc
eduardolluis <1132907@est.intec.edu.do>
2026-03-29 19:12:20 -0400

    show/hide password typing

--- lib/auth/customer_signup.dart
+++ lib/auth/customer_signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_store_app/widgets/auth_widgets.dart';
 
 class CustomerSignup extends StatefulWidget {
   const CustomerSignup({super.key});
@@ -9,6 +10,7 @@ class CustomerSignup extends StatefulWidget {
 }
 
 class _CustomerSignupState extends State<CustomerSignup> {
+  bool passwordVisibility = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,7 +99,21 @@ class _CustomerSignupState extends State<CustomerSignup> {
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 10),
                     child: TextFormField(
+                      obscureText: passwordVisibility,
                       decoration: textFormDecoration.copyWith(
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              passwordVisibility = !passwordVisibility;
+                            });
+                          },
+                          icon: Icon(
+                            passwordVisibility
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.purple,
+                          ),
+                        ),
                         labelText: "Password",
                         hintText: "Enter Your Password",
                       ),
@@ -118,113 +134,3 @@ class _CustomerSignupState extends State<CustomerSignup> {
     );
   }
 }
-
-class AuthButton extends StatelessWidget {
-  final String mainButtonLabel;
-  final Function() onPressed;
-  const AuthButton({
-    super.key,
-    required this.mainButtonLabel,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30),
-      child: Material(
-        color: Colors.purple,
-        borderRadius: BorderRadius.circular(25),
-        child: MaterialButton(
-          minWidth: double.infinity,
-          onPressed: onPressed,
-          child: Text(
-            mainButtonLabel,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class HaveAccount extends StatelessWidget {
-  final String haveAccount;
-  final String actionLabel;
-  final Function() onPressed;
-  const HaveAccount({
-    super.key,
-    required this.haveAccount,
-    required this.actionLabel,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Text(
-          haveAccount,
-          style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
-        ),
-        TextButton(
-          onPressed: onPressed,
-          child: Text(
-            actionLabel,
-            style: TextStyle(
-              color: Colors.purple,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class AuthHeaderLabel extends StatelessWidget {
-  final String headerLabel;
-  const AuthHeaderLabel({super.key, required this.headerLabel});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            headerLabel,
-            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/welcome_screen');
-            },
-            icon: Icon(Icons.home_work, size: 40),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-var textFormDecoration = InputDecoration(
-  labelText: "Full Name",
-  hintText: "Enter Your Full Name",
-  border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-  enabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(25),
-    borderSide: BorderSide(color: Colors.purple, width: 2),
-  ),
-  focusedBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(25),
-    borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
-  ),
-);
