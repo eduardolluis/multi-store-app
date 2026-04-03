ebdeae02f2504e0013ae9593d6587b32edd0ea68
eduardolluis <1132907@est.intec.edu.do>
2026-04-03 09:38:26 -0400

    Streaming stores

--- lib/main_screens/stores.dart
+++ lib/main_screens/stores.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_store_app/widgets/appbar_widgets.dart';
 
@@ -12,6 +13,63 @@ class StoresScreen extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
         title: const AppbarTitle(title: 'Stores'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('suppliers')
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasData) {
+                  return GridView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 25,
+                      crossAxisSpacing: 25,
+                      crossAxisCount: 2,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Stack(
+                            children: [
+                              SizedBox(
+                                height: 120,
+                                width: 120,
+                                child: Image.asset('images/inapp/store.jpg'),
+                              ),
+                              Positioned(
+                                bottom: 28,
+                                left: 10,
+                                child: SizedBox(
+                                  height: 48,
+                                  width: 100,
+                                  child: Image.network(
+                                    snapshot.data!.docs[index]['storeLogo'],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            snapshot.data!.docs[index]['storeName'],
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontFamily: 'AkayaKanadaka',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+                return const Center(child: Text("No Stores"));
+              },
+        ),
       ),
     );
   }
